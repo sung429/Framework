9048471b5e92586750b053a00dbeba1aae35d920
Hyeonmin Seong <sung8504@vtex.co.kr>
2020-12-14 11:09:46 +0900

    스텝6 완료

--- steps/step01.py
+++ steps/step01.py
@@ -1,34 +1,37 @@
+import numpy as np
+
 #step 1
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
         
         
-import numpy as np
-
-# data = np.array(1.0)
-# x = Variable(data)
-# print(x.data)
-
-# x.data = np.array(2.0)
-# print(x.data)
-
-
 #step 2
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        self.input = input
         return output
     
     def forward(self, x):
+        raise NotImplementedError()
+    
+    def backward(self, gy):
         raise NotImplementedError()
 
 
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        y = x ** 2
+        return y
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
         
 # x = Variable(np.array(10))
 # f = Square()
@@ -37,10 +40,16 @@ class Square(Function):
 # print(y.data)
 
 
-#step 3
+#step 3, 6
 class Exp(Function):
     def forward(self, x):
-        return np.exp(x)
+        y = np.exp(x)
+        return y
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
     
 A = Square()
 B = Exp()
@@ -50,8 +59,12 @@ x = Variable(np.array(0.5))
 a = A(x)
 b = B(a)
 y = C(b)
-print(y.data)
 
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad)
 
 # step4
 def numerical_diff(f, x, eps=1e-4):
