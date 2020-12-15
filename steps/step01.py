5688adbd852a4f1a83c60b41f3a8ead8c61281ab
Hyeonmin Seong <sung8504@vtex.co.kr>
2020-12-15 15:10:13 +0900

    8단계 완료

--- steps/step01.py
+++ steps/step01.py
@@ -11,11 +11,14 @@ class Variable:
         self.creator = func
         
     def backward(self):
-        f = self.creator
-        if f is not None:
-            x = f.input
-            x.grad = f.backward(self.grad)
-            x.backward()
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+            
+            if x.creator is not None:
+                funcs.append(x.creator)
         
         
 #step 2
@@ -73,59 +76,7 @@ x = Variable(np.array(0.5))
 a = A(x)
 b = B(a)
 y = C(b)
-print(y.data)
-
-assert y.creator == C
-assert y.creator.input == b
-assert b.creator == B
-assert y.creator.input.creator == B
-assert y.creator.input.creator.input == a
-assert a.creator == A
-assert y.creator.input.creator.input.creator == A
-assert y.creator.input.creator.input.creator.input == x
-
-# y.grad = np.array(1.0)
-# b.grad = C.backward(y.grad)
-# a.grad = B.backward(b.grad)
-# x.grad = A.backward(a.grad)
-# print(x.grad)
-
-# y.grad = np.array(1.0)
-# C = y.creator
-# b = C.input
-# b.grad = C.backward(y.grad)
-# B = b.creator
-# a = B.input
-# a.grad = B.backward(b.grad)
-# A = a.creator
-# x = A.input
-# x.grad = A.backward(a.grad)
-# print(x.grad)
 
 y.grad = np.array(1.0)
 y.backward()
 print(x.grad)
-
-# step4
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
-
-
-# f = Square()
-# x = Variable(np.array(2.0))
-# dy = numerical_diff(f,x)
-# print(dy)
-
-def f(x):
-    A = Square()
-    B = Exp()
-    C = Square()
-    return C(B(A(x)))
-
-x = Variable(np.array(0.5))
-dy = numerical_diff(f, x)
-print(dy)
