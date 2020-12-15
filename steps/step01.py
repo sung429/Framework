270e240e00d517e37f5e87eaad5db17fa351c3e2
Hyeonmin Seong <sung8504@vtex.co.kr>
2020-12-15 15:53:58 +0900

    10단계 완료

--- steps/step01.py
+++ steps/step01.py
@@ -89,3 +89,21 @@ y = square(exp(square(x)))
 # y.grad = np.array(1.0)
 y.backward()
 print(x.grad)
+
+
+import unittest
+
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+    
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
